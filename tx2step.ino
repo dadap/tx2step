d3ee076ea1c51555c9bb1768f5403a467ec85700
Daniel Dadap <daniel@dadap.net>
2017-02-24 22:31:14 -0600

    Add LICENSE file
    Also add a newline between the project name and the copyright line in
    the comment at the top of tx2step.ino.

--- tx2step.ino
+++ tx2step.ino
@@ -1,6 +1,7 @@
 /*
  * Texas Two Step:
  *     A dual axis stepper motor controller for equatorial telescope mounts
+ *
  * Copyright (c) 2017 Daniel Dadap
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
