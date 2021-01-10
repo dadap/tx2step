9be5e284bea2b0c24c02996fcd3e4ac24203ba1d
Daniel Dadap <daniel@dadap.net>
2021-01-09 21:28:35 -0600

    Reduce maximum rate to 40x
    Setting is not quite stable at 48x; 40x works more reliably.

--- tx2step.ino
+++ tx2step.ino
@@ -145,8 +145,8 @@ static st4_input st4_inputs[] = {
 
 /* Table of setting rates, in units of 1/4 tracking rate, e.g.,
  * a rate value of "12" represents 3x tracking rate. */
-const int setting_rates[] = {-192, -80, -40, -12, -4, -2, -1,
-                             0, 0, 1, 2, 4, 12, 40, 80, 192};
+const int setting_rates[] = {-160, -80, -40, -12, -4, -2, -1,
+                             0, 0, 1, 2, 4, 12, 40, 80, 160};
 
 /* Table of guiding rates, in same units as setting rates. Duplicated values
  * for rates < 1x tracking are to ensure that 1x tracking is at the midpoint. */
